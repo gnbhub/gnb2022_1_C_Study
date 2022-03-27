965efc5174528e8e35c69704cd96ca72a92f169e
dnd9976 <101636072+dnd9976@users.noreply.github.com>
2022-03-27 16:54:58 +0900

    Create 김새웅 과제3.c

---
+++ week1/김새웅 과제3.c
@@ -0,0 +1,16 @@
+//2022113020 김새웅
+```c
+#include <stdio.h>
+int main(void)
+{
+	int age = 0;
+	
+	printf("양의 정수를 입력하시오 :");
+		scanf_s("%d", &age);
+	printf("|number      |decimal     |octal       |hexadecimal |\n");
+	printf("|%-12d|%-12d|%-12o|%-12x|", age, age, age, age);
+
+
+	return 0;
+}
+```c
